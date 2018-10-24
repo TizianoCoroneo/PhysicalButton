82859f054ebb13b85922b3baf5d8f35d344a37a2
Tiziano Coroneo <tizianocoroneo@me.com>
2018-10-24 19:13:59 +0200

    Implemented shadow

--- Example/PhysicalButton/PhysicalButton.swift
+++ Example/PhysicalButton/PhysicalButton.swift
@@ -13,12 +13,57 @@ import PhysicalButton
 open class PhysicalButton: ButtonDoneRight {
 
     var feedbackStyle: UIImpactFeedbackGenerator.FeedbackStyle = .heavy
-    var rotationIntensity: CGFloat = (90/180) * (.pi / 2)
-    var translationIntensity: CGFloat = 150
-    var scaleIntensity: CGFloat = 0.95
+    var rotationIntensity: CGFloat = (45/180) * (.pi / 2)
+    var translationIntensity: CGFloat = 75
+    var scaleIntensity: CGFloat = 0.05
+
+    var pressedShadowScale: CGFloat = 0.9
+
+    var normalShadowRadius: CGFloat = 10
+    var pressedShadowRadius: CGFloat = 4
+
+    var defaultTouchRelativeForce: CGFloat = 0.7
+
+    var minimumForceFactor: CGFloat = 0.5
+    var maximumForceFactor: CGFloat = 5
 
     private lazy var feedbackGenerator = UIImpactFeedbackGenerator(style: feedbackStyle)
     private var isTouchActive: Bool = false
+
+    var isForceTouchEnabled: Bool {
+        return traitCollection.forceTouchCapability == .available
+    }
+
+    lazy var shadowLayer3D: CAShapeLayer = {
+        let layer = CAShapeLayer()
+        layer.frame = self.bounds
+        layer.shadowPath = self.roundedPath.cgPath
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOffset = .zero
+        layer.shadowRadius = 10
+        layer.shadowOpacity = 0.3
+        layer.zPosition = -200
+        return layer
+    }()
+
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+
+    private func setup() {
+
+        layer.insertSublayer(shadowLayer3D, at: 0)
+
+        imageLayer.zPosition = -100
+        shapeLayer.zPosition = -200
+        shadowLayer3D.zPosition = -300
+    }
 
     override open func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard event?.type == .touches,
@@ -28,8 +73,9 @@ open class PhysicalButton: ButtonDoneRight {
 
         isTouchActive = true
         feedbackGenerator.impactOccurred()
+        shadowLayer3D.shadowRadius = pressedShadowRadius
 
-        layer.transform = transform
+        apply(transform: transform)
     }
 
     open override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -42,28 +88,32 @@ open class PhysicalButton: ButtonDoneRight {
             else {
                 isTouchActive = false
                 feedbackGenerator.impactOccurred()
+                shadowLayer3D.shadowRadius = normalShadowRadius
                 releaseTransform()
                 return
         }
 
-        self.layer.transform = transform
+        apply(transform: transform)
     }
 
     override open func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard isTouchActive
             else { return }
 
+        shadowLayer3D.shadowRadius = normalShadowRadius
         releaseTransform()
     }
 
     func transformFromTouch(_ touch: UITouch) -> CATransform3D? {
-//        let superviewLocation = touch.preciseLocation(in: superview)
-//
-//        let location = CGPoint(
-//            x: superviewLocation.x - frame.origin.x,
-//            y: superviewLocation.y - frame.origin.y)
 
         let location = touch.preciseLocation(in: self)
+
+        let force: CGFloat
+        if isForceTouchEnabled {
+            force = max(min(sqrt(touch.force), maximumForceFactor), minimumForceFactor)
+        } else {
+            force = defaultTouchRelativeForce
+        }
 
         guard
             self.bounds.contains(location)
@@ -90,12 +140,12 @@ open class PhysicalButton: ButtonDoneRight {
         assert(relativeStrenght >= 0)
 
         let rotation = CATransform3DMakeRotation(
-            relativeStrenght * rotationIntensity,
+            relativeStrenght * rotationIntensity * force,
             relativeDistanceFromCenter.y,
             relativeDistanceFromCenter.x,
             0)
 
-        let translationFactor = (1 - cos(relativeStrenght * rotationIntensity)) * translationIntensity
+        let translationFactor = (1 - cos(relativeStrenght * rotationIntensity * force)) * translationIntensity
 
         let translation = CATransform3DTranslate(
             rotation,
@@ -103,13 +153,39 @@ open class PhysicalButton: ButtonDoneRight {
             -relativeDistanceFromCenter.y * translationFactor,
             0)
 
-        let scale = CATransform3DScale(translation, scaleIntensity, scaleIntensity, 1)
+        let scaleFactor = 1 - (scaleIntensity * relativeStrenght * force)
+
+        let scale = CATransform3DScale(
+            translation,
+            scaleFactor,
+            scaleFactor,
+            1)
 
         return scale
     }
 
+    func apply(transform: CATransform3D) {
+        CATransaction.begin()
+        layer.sublayers?.forEach { layer in
+            layer.transform = transform
+            layer.removeAllAnimations()
+        }
+
+        shadowLayer3D.transform = CATransform3DScale(
+            CATransform3DInvert(transform),
+            pressedShadowScale,
+            pressedShadowScale,
+            1)
+        shadowLayer3D.removeAllAnimations()
+
+        CATransaction.commit()
+    }
+
     func releaseTransform() {
         UIView.animate(withDuration: 0.1) { [weak self] in
+            self?.layer.sublayers?.forEach { layer in
+                layer.transform = CATransform3DIdentity
+            }
             self?.layer.transform = CATransform3DIdentity
         }
     }
